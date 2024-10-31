854e6fd2a79091cf7b8f7d08d2027f57c991f1fe
ShiharaKaumadi <eshani9544@gmail.com>
2024-10-31 12:22:59 +0530

    create DBConnection class

--- src/lk/ijse/chat/db/DBConnection.java
+++ src/lk/ijse/chat/db/DBConnection.java
@@ -1,4 +1,20 @@
 package lk.ijse.chat.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    public static DBConnection dbConnection;
+    public Connection connection;
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chitchat?useSSL=false&allowPublicKeyRetrieval=true");
+    }
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbConnection==null)?dbConnection=new DBConnection():dbConnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
 }
