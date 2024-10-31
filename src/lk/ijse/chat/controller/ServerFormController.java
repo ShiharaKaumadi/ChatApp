de18e4b4c6e308875991e2c075c5f93486676527
ShiharaKaumadi <eshani9544@gmail.com>
2024-10-31 13:39:26 +0530

    Server Form Created

---
+++ src/lk/ijse/chat/controller/ServerFormController.java
@@ -0,0 +1,26 @@
+package lk.ijse.chat.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class ServerFormController {
+    public Pane chatPane;
+    public Button btnEmoji;
+    public TextField txtMessage;
+    public Button btnAttachFile;
+    public Button btnSend;
+
+    public void btnEmojiOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtMessegeEnterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAttachFileOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
