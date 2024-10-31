4f56f1539a69c928905fb17158aef142ea932084
ShiharaKaumadi <eshani9544@gmail.com>
2024-10-31 13:58:41 +0530

    Server COntroller Created

--- src/lk/ijse/chat/controller/ServerFormController.java
+++ src/lk/ijse/chat/controller/ServerFormController.java
@@ -5,12 +5,37 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerFormController {
     public Pane chatPane;
     public Button btnEmoji;
     public TextField txtMessage;
     public Button btnAttachFile;
     public Button btnSend;
+
+    public void initialize(){
+        new Thread(()->{
+            try{
+                ServerSocket socket = new ServerSocket(3000);
+                Socket accept = socket.accept();
+                System.out.println("Server Started");
+                System.out.println("Client Connected");
+                InputStreamReader isr = new InputStreamReader(accept.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(isr);
+                String string = bufferedReader.readLine();
+                System.out.println(string);
+            }catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }).start();
+    }
+
+
 
     public void btnEmojiOnAction(ActionEvent actionEvent) {
     }
